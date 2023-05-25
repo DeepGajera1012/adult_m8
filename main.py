0c475128c3094417ec66f20e5c0aa996e1622fda
DeepGajera1012 <deepgajera11@gmail.com>
2023-05-25 10:53:10 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from project import app
+# cur = g.db.cursor(dictionary=True)
+if __name__ == '__main__':
+    
+    app.run()
+
+
---
+++ project/client/client.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify, request, g
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+client_bp = Blueprint('client', __name__)
+
+
+    
+    
+
+
+    
