b9e5b98c03ece5c43cbe0b8d23ee2300a1e91ea6
DeepGajera1012 <95013271+DeepGajera1012@users.noreply.github.com>
2023-05-26 14:39:44 +0530

    Delete project directory

--- project/escort/escort.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, jsonify, request, g
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from datetime import datetime
-
-escort_bp = Blueprint('escort', __name__)
-
